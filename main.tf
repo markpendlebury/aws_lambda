37ecb3c93fa70a40d3c7679abd4e932065b3cc9a
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-03 21:28:37 +0100

    Adding logic

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ resource "aws_lambda_function" "this" {
   s3_bucket        = var.package_type == "Zip" ? var.s3_bucket : null
   s3_key           = var.package_type == "Zip" ? var.s3_key : null
   filename         = var.package_type == "Zip" ? null : var.filename
-  handler          = var.lambda_handler
-  runtime          = var.runtime
-  timeout          = var.timeout
-  memory_size      = var.memory_size
-  role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = var.source_code_hash
+  source_code_hash = var.package_type == "Zip" ? var.source_code_hash : null
+
+  handler     = var.lambda_handler
+  runtime     = var.runtime
+  timeout     = var.timeout
+  memory_size = var.memory_size
+  role        = aws_iam_role.lambda_exec.arn
 
   environment {
     variables = var.environment
