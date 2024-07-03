3d27e81f3777860983b28f1a8124b2ba5cc90019
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-03 21:24:53 +0100

    adding a hash check

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 resource "aws_lambda_function" "this" {
-  function_name = "${var.service_name}-lambda"
-  package_type  = var.package_type
-  s3_bucket     = var.package_type == "Zip" ? var.s3_bucket : null
-  s3_key        = var.package_type == "Zip" ? var.s3_key : null
-  filename      = var.package_type == "Zip" ? null : var.filename
-  handler       = var.lambda_handler
-  runtime       = var.runtime
-  timeout       = var.timeout
-  memory_size   = var.memory_size
-  role          = aws_iam_role.lambda_exec.arn
+  function_name    = "${var.service_name}-lambda"
+  package_type     = var.package_type
+  s3_bucket        = var.package_type == "Zip" ? var.s3_bucket : null
+  s3_key           = var.package_type == "Zip" ? var.s3_key : null
+  filename         = var.package_type == "Zip" ? null : var.filename
+  handler          = var.lambda_handler
+  runtime          = var.runtime
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  role             = aws_iam_role.lambda_exec.arn
+  source_code_hash = var.package_type == "Zip" ? filebase64sha256(var.filename) : null
 
   environment {
     variables = var.environment
