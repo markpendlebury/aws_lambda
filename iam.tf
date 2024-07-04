07c90ffc9db8285ea194fb76ba940f788b4543cc
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 18:00:02 +0100

    .

--- iam.tf
+++ iam.tf
@@ -1,6 +1,21 @@
+resource "aws_iam_role" "this" {
+  name = "${var.service_name}-lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      }
+    ]
+  })
+}
+
+
+
 data "aws_iam_policy_document" "this" {
-
-
   statement {
     actions = [
       "logs:CreateLogGroup",
@@ -13,25 +28,20 @@ data "aws_iam_policy_document" "this" {
 
 }
 
+
 resource "aws_iam_policy" "this" {
   name        = "${var.service_name}-lambda-basic-execution-policy"
   description = "IAM policy for basic execution role for Lambda"
   policy      = data.aws_iam_policy_document.this.json
 }
 
-resource "aws_iam_role" "this" {
-  name               = "${var.service_name}-lambda-exec-role"
-  assume_role_policy = data.aws_iam_policy_document.this.json
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
 }
+
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.assume_role_policy.arn
-}
-
