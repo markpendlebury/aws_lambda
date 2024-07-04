8e13aca2dfd6de1d46c0e62db8697aa47dfd9f57
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 16:50:20 +0100

    tidying up

--- iam.tf
+++ iam.tf
@@ -1,21 +1,14 @@
-resource "aws_iam_role" "lambda_exec" {
-  name = "${var.service_name}-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
-}
-
 data "aws_iam_policy_document" "lambda_policy" {
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+
   statement {
     actions = [
       "logs:CreateLogGroup",
@@ -26,23 +19,13 @@ data "aws_iam_policy_document" "lambda_policy" {
     resources = ["arn:aws:logs:*:*:*"]
     effect    = "Allow"
   }
+
 }
 
 resource "aws_iam_policy" "lambda_policy" {
   name        = "${var.service_name}-lambda-policy"
   description = "A policy for the lambda function"
   policy      = data.aws_iam_policy_document.lambda_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy_document.lambda_policy.arn
-}
-
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy_document.lambda_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
