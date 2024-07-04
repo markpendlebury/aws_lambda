04324fae9f218c20fbc2f569bc82cb94ff1dcbca
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 17:28:43 +0100

    derp

--- iam.tf
+++ iam.tf
@@ -10,6 +10,15 @@ data "aws_iam_policy_document" "this" {
     resources = ["arn:aws:logs:*:*:*"]
     effect    = "Allow"
   }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_policy" "this" {
