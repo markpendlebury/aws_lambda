e4c3e1d9806c8c687c35418b384a1d50c39fb08f
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 17:05:26 +0100

    maybe this one?

--- iam.tf
+++ iam.tf
@@ -1,5 +1,4 @@
-data "aws_iam_policy_document" "lambda_policy" {
-
+data "aws_iam_policy_document" "this" {
   statement {
     actions = ["sts:AssumeRole"]
     effect  = "Allow"
@@ -15,20 +14,30 @@ data "aws_iam_policy_document" "lambda_policy" {
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-
     resources = ["arn:aws:logs:*:*:*"]
     effect    = "Allow"
   }
-
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "${var.service_name}-lambda-policy"
-  description = "A policy for the lambda function"
-  policy      = data.aws_iam_policy_document.lambda_policy.json
+resource "aws_iam_policy" "this" {
+  name        = "${var.service_name}-lambda-basic-execution-policy"
+  description = "IAM policy for basic execution role for Lambda"
+  policy      = data.aws_iam_policy_document.this.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
+resource "aws_iam_role" "this" {
+  name               = "${var.service_name}-lambda-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
+}
+resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
