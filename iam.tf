7be63855496688892333e9df18e8e3affde46717
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 18:04:57 +0100

    case sensetive innit

--- iam.tf
+++ iam.tf
@@ -2,7 +2,7 @@ resource "aws_iam_role" "this" {
   name = "${var.service_name}-lambda-exec-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    actions = ["sts:AssumeRole"]
+    Actions = ["sts:AssumeRole"]
     Statement = [
       {
         Effect = "Allow",
