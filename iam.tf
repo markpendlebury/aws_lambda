b10e183dc710cfcc4c368349f54591e786f9a8a9
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 17:53:11 +0100

    test

--- iam.tf
+++ iam.tf
@@ -13,26 +13,6 @@ data "aws_iam_policy_document" "this" {
 
 }
 
-resource "aws_iam_role" "assume_role" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "lambda.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_policy" "assume_role_policy" {
-  name        = "${var.service_name}-lambda-assume-role-policy"
-  description = "IAM policy for basic execution role for Lambda"
-  policy      = aws_iam_role.assume_role.json
-}
-
 resource "aws_iam_policy" "this" {
   name        = "${var.service_name}-lambda-basic-execution-policy"
   description = "IAM policy for basic execution role for Lambda"
