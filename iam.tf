ceb7684b294b24ce9e03d7c79e22cb144c54a7d0
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-07-04 17:33:03 +0100

    .

--- iam.tf
+++ iam.tf
@@ -29,31 +29,9 @@ resource "aws_iam_policy" "this" {
 
 resource "aws_iam_role" "this" {
   name               = "${var.service_name}-lambda-exec-role"
-  assume_role_policy = aws_iam_role_policy.assume_role.policy
+  assume_role_policy = data.aws_iam_policy_document.this.json
 }
 
-
-resource "aws_iam_role_policy" "assume_role" {
-  name = "${var.service_name}-lambda-assume-role"
-  role = aws_iam_role.this.name
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_role_policy_attachment" "this" {
-  role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.this.arn
-}
 resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
